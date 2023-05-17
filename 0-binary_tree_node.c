e1eda49e650ef2e1943e2372cfab733703a8b9c6
Sonnydread <alvaradoguille590@gmail.com>
2023-05-16 19:55:16 -0700

    0-bin

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,30 +8,28 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 {
-
 binary_tree_t *new;
 
+if (parent == NULL)
+{
 parent = malloc(sizeof(binary_tree_t));
 if (parent == NULL)
 return (NULL);
-
 parent->n = value;
 parent->parent = NULL;
 parent->right = NULL;
 parent->left = NULL;
 return (parent);
+}
 
 new = malloc(sizeof(binary_tree_t));
 if (new == NULL)
-
-free(parent);
 return (NULL);
 
 new->n = value;
 new->parent = parent;
 new->right = NULL;
 new->left = NULL;
+
 return (new);
-
 }
-
