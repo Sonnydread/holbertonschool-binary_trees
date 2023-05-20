c85712b112077362543bf8d57f08c9719a685303
Sonnydread <alvaradoguille590@gmail.com>
2023-05-20 05:34:21 -0700

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,14 +13,11 @@ int left = 0, right = 0;
 
 if (tree == NULL)
 return (0);
-
 if (tree->left)
-left += 1 + binary_tree_height(tree->left);
-if (tree->right)
-right += 1 + binary_tree_height(tree->right);
-if (right > left)
-
-return (left);
-return (right);
-
+left = binary_tree_height(tree->left);
+right = binary_tree_height(tree->right);
+if (left > right)
+return (left + 1);
+else
+return (right + 1);
 }
